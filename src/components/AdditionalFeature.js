bf711bf816ba551dcbb36c37185aeae25c0e45c6
Lambda_School_Loaner_198 <lambda_school_loaner_198@Lambda-School-Loaner-198s-MacBook-Pro.local>
2020-01-22 14:01:07 -0800

    mvp complete

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+
+import {addFeature} from '../actions/carActions'
+
 
 const AdditionalFeature = props => {
+  const dispatch = useDispatch();
+
+  const handleAddFeature = () => {
+  dispatch(addFeature(props.feature));
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button className="button" onClick={(handleAddFeature)}>Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
